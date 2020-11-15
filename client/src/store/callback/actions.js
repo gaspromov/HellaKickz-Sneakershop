313f6713dd970abcdeee0f476d92758a00f8d3f7
MrMargaryan <hamlet.java@gmail.com>
2020-11-15 19:27:00 +0300

    added callback features

---
+++ client/src/store/callback/actions.js
@@ -0,0 +1,79 @@
+import {
+  FETCH_CALLBACKS_REQUEST,
+  FETCH_CALLBACKS_SUCCESS,
+  FETCH_CALLBACKS_FAIL,
+  READ_CALLBACK_REQUEST,
+  READ_CALLBACK_SUCCESS,
+  READ_CALLBACK_FAIL,
+  DELETE_CALLBACK_REQUEST,
+  DELETE_CALLBACK_SUCCESS,
+  DELETE_CALLBACK_FAIL
+} from './types'
+import axios from 'axios'
+import cookie from 'js-cookie'
+
+export const fetchCallbacks = () => async (dispatch) => {
+  dispatch({ type: FETCH_CALLBACKS_REQUEST })
+
+  try {
+    const token = cookie.getJSON('accessToken')
+
+    const { data } = await axios.get('/api/v1/callbacks', {
+      headers: {
+        'Authorization': `Basic ${token.accessToken}`
+      }
+    })
+
+    dispatch({
+      type: FETCH_CALLBACKS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: FETCH_CALLBACKS_FAIL,
+      payload: error.message
+    })
+  }
+}
+
+export const readCallback = (id) => async (dispatch) => {
+  dispatch({ type: READ_CALLBACK_REQUEST })
+
+  try {
+    const token = cookie.getJSON('accessToken')
+
+    await axios.put(`/api/v1/callbacks/${id}`, {}, {
+      headers: {
+        'Authorization': `Basic ${token.accessToken}`
+      }
+    })
+
+    dispatch({ type: READ_CALLBACK_SUCCESS })
+  } catch (error) {
+    dispatch({
+      type: READ_CALLBACK_FAIL,
+      payload: error.message
+    })
+  }
+}
+
+export const deleteCallback = (id) => async (dispatch) => {
+  dispatch({ type: DELETE_CALLBACK_REQUEST })
+
+  try {
+    const token = cookie.getJSON('accessToken')
+
+    await axios.delete(`/api/v1/callbacks/${id}`, {
+      headers: {
+        'Authorization': `Basic ${token.accessToken}`
+      }
+    })
+
+    dispatch({ type: DELETE_CALLBACK_SUCCESS })
+  } catch (error) {
+    dispatch({
+      type: DELETE_CALLBACK_FAIL,
+      payload: error.message
+    })
+  }
+}
---
+++ client/src/store/callback/reducers.js
@@ -0,0 +1,109 @@
+import {
+  FETCH_CALLBACKS_REQUEST,
+  FETCH_CALLBACKS_SUCCESS,
+  FETCH_CALLBACKS_FAIL,
+  READ_CALLBACK_REQUEST,
+  READ_CALLBACK_SUCCESS,
+  READ_CALLBACK_FAIL,
+  DELETE_CALLBACK_REQUEST,
+  DELETE_CALLBACK_SUCCESS,
+  DELETE_CALLBACK_FAIL
+} from './types'
+
+const fetchCallbacksInitialState = {
+  loading: false,
+  loaded: false,
+  error: null,
+  entities: []
+}
+
+export const fetchCallbacksReducer = (state = fetchCallbacksInitialState, action) => {
+  switch (action.type) {
+    case FETCH_CALLBACKS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_CALLBACKS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: null,
+        entities: action.payload
+      }
+    case FETCH_CALLBACKS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+const readCallbackInitialState = {
+  loading: false,
+  loaded: false,
+  error: null
+}
+
+export const readCallbackReducer = (state = readCallbackInitialState, action) => {
+  switch (action.type) {
+    case READ_CALLBACK_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case READ_CALLBACK_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: null
+      }
+    case READ_CALLBACK_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+const deleteCallbackInitialState = {
+  loading: false,
+  loaded: false,
+  error: null
+}
+
+export const deleteCallbackReducer = (state = deleteCallbackInitialState, action) => {
+  switch (action.type) {
+    case DELETE_CALLBACK_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case DELETE_CALLBACK_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: null
+      }
+    case DELETE_CALLBACK_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
