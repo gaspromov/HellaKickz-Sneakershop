2858fea2d3aaa62629a53f417d1aca9c5fa3c559
gaspromov <gaspromov@mail.ru>
2020-11-20 00:57:39 +0300

    Fix border for hover menu-link, animation for subMenu, hover btn styes

--- client/src/components/MobileMenu/MobileMenu.js
+++ client/src/components/MobileMenu/MobileMenu.js
@@ -9,7 +9,7 @@ const MobileMenu = ({ isOpen, onMobileMenuLinkClick }) => {
   }
 
   return (
-    <nav style={{ display: isOpen ? 'flex' : 'none' }} className={styles.nav}>
+    <nav style={{ transform: isOpen ? 'scale(1,1)' : 'scale(1,0)' }} className={styles.nav}>
       <ul className={styles.menu}>
         <li className={styles.menuItem}>
           <NavLink to="/" onClick={onMenuLinkClick} className={styles.menuLink}>Каталог</NavLink>
